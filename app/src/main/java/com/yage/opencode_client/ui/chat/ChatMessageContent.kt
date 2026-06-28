e964841fa550b5605b631b1441c9d1764b0bdabb
mfreceiver <mfreceiver@gmail.com>
2026-06-28 22:35:58 +0800

    chore: 移除未使用的derivedStateOf import(dser NIT)

--- app/src/main/java/com/yage/opencode_client/ui/chat/ChatMessageContent.kt
+++ app/src/main/java/com/yage/opencode_client/ui/chat/ChatMessageContent.kt
@@ -48,7 +48,7 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
+
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
