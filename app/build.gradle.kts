36fead2fa5241b999ee250f71eb95a65e6ef8b84
mfreceiver <mfreceiver@gmail.com>
2026-07-04 12:07:38 +0800

    release: 0.3.3
    versionName: 0.3.3
    versionCode: 29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "cn.vectory.ocdroid"
         minSdk = 26
         targetSdk = 34
-        versionCode = 28
-        versionName = "0.3.2"
+        versionCode = 29
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // Integration test credentials from .env (dynamic, not in code)
